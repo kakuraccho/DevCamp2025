84554df7065970ad3db63cc0c0a2115a663262fc
kakuraccho <kakurattyo1216@gmail.com>
2025-09-11 10:53:13 +0900

    add check session for regist page

--- app/src/pages/FAMRegistPage.tsx
+++ app/src/pages/FAMRegistPage.tsx
@@ -1,9 +1,16 @@
-import { useParams } from "react-router-dom"
+import { useParams, Navigate } from "react-router-dom"
 import FAMRegist from "../components/FAMRegist";
+import useAuth from "../hooks/useAuth";
 
 export default function FAMRegistPage() {
     const param = useParams()
     const famId = param.id ?? "";
+
+    const { session } = useAuth()
+
+    if (!session) {
+        return <Navigate to={'/'} replace />
+    }
 
     return (
         <FAMRegist id={famId} />
--- app/src/pages/LUMRegistPage.tsx
+++ app/src/pages/LUMRegistPage.tsx
@@ -1,9 +1,16 @@
-import { useParams } from "react-router-dom"
+import { useParams, Navigate } from "react-router-dom"
+import useAuth from "../hooks/useAuth";
 import LUMRegist from "../components/LUMRegist"
 
 export default function LUMRegistPage() {
     const param = useParams()
     const lumId = param.id ?? "";
+
+    const { session } = useAuth()
+
+    if (!session) {
+        return <Navigate to={'/'} replace />
+    }
 
     return (
         <LUMRegist id={lumId} />
