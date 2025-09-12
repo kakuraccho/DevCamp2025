81f3b14e86317c399b2ccb1e06e8b2777dd04dac
kakuraccho <kakurattyo1216@gmail.com>
2025-09-12 22:45:29 +0900

    improve logout

--- app/src/pages/LogoutPage.tsx
+++ app/src/pages/LogoutPage.tsx
@@ -1,14 +1,16 @@
 import { useNavigate } from "react-router-dom"
-import { supabase } from "../supabaseClient"
+import useAuth from "../hooks/useAuth"
+import { useEffect } from "react"
 
 export default function LogoutPage() {
     const navigate = useNavigate()
-    const logout = async () => {
-        await supabase.auth.signOut()
-        navigate('/')
-    }
-    
-    logout()
+    const { logout, loading } = useAuth()
+
+    useEffect(() => {
+        logout()
+
+        if (!loading) navigate('/')
+    })
 
     return (
         <p>logouting...</p>
