4f9c2f43f58993e3bcc935fff4f8c617f04e8157
kakuraccho <kakurattyo1216@gmail.com>
2025-09-12 11:15:05 +0900

    improve for deploy 2

--- app/src/pages/FAMPage.tsx
+++ app/src/pages/FAMPage.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom"
 import FAM from "../components/FAM"
 
 export default function FAMPage() {
--- app/src/pages/LUMPage.tsx
+++ app/src/pages/LUMPage.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom"
 import LUM from "../components/LUM";
 
 export default function LUMPage() {
